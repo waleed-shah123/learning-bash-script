cc8f6bbb92ab49c2c59782f8b58686fc4439ecf5
waleed-shah123 <waleed.shahchohan@gmail.com>
2022-12-10 15:00:21 +0500

    second script

---
+++ script2.sh
@@ -0,0 +1,14 @@
+mkdir countries
+
+cd countries
+
+mkdir USA India UK
+
+echo " washington DC" > USA/capital.txt
+
+echo "London" > UK/capital.txt
+
+echo "New Delhi" > India/capital.txt
+
+uptime
+
