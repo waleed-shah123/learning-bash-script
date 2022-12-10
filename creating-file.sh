f151d1b15243fbfe0adaaa7358376b0acc9a8049
waleed-shah123 <waleed.shahchohan@gmail.com>
2022-12-10 15:57:34 +0500

    This file is use to create files through script

---
+++ creating-file.sh
@@ -0,0 +1,16 @@
+$FILE01="Canda"
+$FILE02="America"
+$FILE03="India"
+
+cd  /home/waleed
+
+echo "creating file called $FILE01" 
+touch $FILE01
+
+echo "creating file called $FILE02"
+touch $FILE02
+
+echo "creating file called $FILE03"
+touch $FILE03
+
+
