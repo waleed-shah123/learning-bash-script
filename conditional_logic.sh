db1eef0ad250f1b5a12c56feba7afce9ff4c4ce6
waleed-shah123 <waleed.shahchohan@gmail.com>
2022-12-10 19:56:10 +0500

    checking directory existance

---
+++ conditional_logic.sh
@@ -0,0 +1,25 @@
+if [ -d "/home/waleed/Documents/learning_bash_script"]
+
+then
+  echo "Directory exist"
+
+else
+
+  echo "Directory dose not exist"
+
+fi
+
+
+
+
+
+
+
+#user= echo $USER
+
+#if [user = "waleed"]
+
+#then
+
+#   echo"you are good to go"
+#fi
